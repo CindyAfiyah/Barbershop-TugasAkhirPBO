22a7dbaa85c4490f76db81b75a380c459e38dcf5
CindyAfiyah <cindycurup17@gmail.com>
2022-12-28 14:41:52 +0700

    first commit

---
+++ StructLayanan.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class StructLayanan {
+    public String idLayanan;
+    private String namaLayanan;
+    private double harga;
+
+    public void setNamaLayanan(String serviceName){
+        namaLayanan = serviceName;
+    }
+    public String getNamaLayanan(){
+        return namaLayanan;
+    }
+    public void setHarga(double price){
+        harga = price;
+    }
+    public double getPrice(){
+        return harga;
+    }
+}
